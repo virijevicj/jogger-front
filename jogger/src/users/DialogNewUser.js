be8b6e041aabd5019e7b15dc7c43f7f5b9bbf044
virijevicj <89025058+virijevicj@users.noreply.github.com>
2024-02-02 19:41:23 +0100

    dialog za dodavanje novog user-a

---
+++ jogger/src/users/DialogNewUser.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import NewUser from './NewUser'
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function CustomizedDialogs({roles}) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const rolesArray = roles.map(r => r.name);
+
+  return (
+    <React.Fragment>
+      <Button variant="contained" sx={{ bgcolor: '#1976d2'}} onClick={handleClickOpen}>
+        Nov korisnik
+      </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+            <NewUser roles={rolesArray}></NewUser>
+        </DialogContent>
+      </BootstrapDialog>
+    </React.Fragment>
+  );
+}
