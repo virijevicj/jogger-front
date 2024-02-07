931a0a4a97874f3151e4d670e312f83efd3f348d
virijevicj <89025058+virijevicj@users.noreply.github.com>
2024-02-07 16:45:53 +0100

    prosiren navigacioni meni

--- jogger/src/menu/NavBar.js
+++ jogger/src/menu/NavBar.js
@@ -6,19 +6,46 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { Button } from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
 
 export default function MenuAppBar() {
+  const navigate=useNavigate();
   const roles = localStorage.getItem('Roles');
+  const handleKorisniciClick = () => {
+    if (roles.includes("Admin"))
+    navigate('/users');
+  } 
+  const handleMaterijaliClick = () => {
+    if (roles.includes("Developer") || roles.includes("Intern"))
+    navigate('/learning-materials');
+  } 
+
+  const handleLogout = (event) => {
+    event.preventDefault(); 
+    const token = localStorage.getItem('Token');
+    axios.post('http://localhost:9000/jogger/api/v1/auth/logout', "", {
+      headers : {
+        'Token' : token
+      },
+    })
+    .then((response) => {
+      localStorage.clear();
+      navigate('/');
+    })
+    .catch((error) => console.log(error))
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          {roles.includes("Developer") && <Button color="inherit">Materijali</Button>}
-          {roles.includes("Admin") && <Button color="inherit">Korisnici</Button>}
+          {roles.includes("Developer") || roles.includes("Intern") && <Button color="inherit" onClick={handleMaterijaliClick}>Materijali</Button>}
+          {roles.includes("Admin") && <Button color="inherit" onClick={handleKorisniciClick}>Korisnici</Button>}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           </Typography>
           <IconButton aria-label="delete" size="large">
-                <ExitToAppIcon titleAccess='Odjava' fontSize="inherit" color="inherit"/>
+                <ExitToAppIcon onClick={handleLogout} titleAccess='Odjava' fontSize="inherit" color="inherit"/>
           </IconButton>
         </Toolbar>
       </AppBar>
