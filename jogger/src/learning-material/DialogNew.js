728dbe585e68304cb70cf9427d5c6232e564c5b0
virijevicj <89025058+virijevicj@users.noreply.github.com>
2024-02-07 17:00:58 +0100

    ups dopuna za dijalog za nove materijale za ucenje

---
+++ jogger/src/learning-material/DialogNew.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import NewLM from './NewLM'
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function DialogSearch({props}) {
+    const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="contained" sx={{ bgcolor: '#1976d2'}} onClick={handleClickOpen}>
+        Nov materijal
+      </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+            <NewLM props={props}></NewLM>
+        </DialogContent>
+      </BootstrapDialog>
+    </React.Fragment>
+  );
+}
