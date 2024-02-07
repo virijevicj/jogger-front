bfe56778fcaf681507406880819a12ba50543d02
virijevicj <89025058+virijevicj@users.noreply.github.com>
2024-02-07 16:41:49 +0100

    Dodate nove rute za korisnike i materijale za ucenje

--- jogger/src/App.js
+++ jogger/src/App.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SignInSide from "./login/SignInSide";
+import Users from './users/Users'
+import LM from './learning-material/LearningMaterial';
 
 function App() {
   return (
     <Routes>
     <Route path="/" element={<SignInSide />} />
+    <Route path="/users" element={< Users/>} />  
+    <Route path="learning-materials" element={<LM/>}/>
   </Routes>
   );
 }
